422b86a65c480668c0bf3240c61db785e5416d87
Suphakit Khampan <skh@manaosoftware.com>
2020-01-20 17:49:11 +0700

    Project Present.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.Users;
+
+namespace WebApplication1.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult ShowUser(User users)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View();
+
+                ViewData["Name"] = users.Name;
+                ViewData["Address"] = users.Address;
+                ViewData["City"] = users.City;
+                ViewData["Province"] = users.Province;
+                ViewData["Country"] = users.Country;
+                ViewData["Zipcode"] = users.Zipcode;
+                ViewData["Email"] = users.Email;
+                ViewData["Gender"] = users.Gender;
+
+                return View("ShowUser");
+            } catch
+            {
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ Models/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Users
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+        public string Zipcode { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+    }
+}
